40e82e5a0136e05e74f6e4e76bcc6688c94ac6d2
Adam Paternostro <AdamPaternostro@users.noreply.github.com>
2017-08-09 11:31:40 -0400

    Add files via upload

---
+++ UploadLocalFolderToADLS.ps1
@@ -0,0 +1,23 @@
+﻿# Change these
+$destinationDataLakeAccount = "mydatalakestore"
+$destinationDatalakefolder = "Raw"
+$subscriptionId = "00000000-0000-0000-0000-000000000000"
+
+# Login Items
+Login-AzureRmAccount 
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+ 
+# Upload to Data Lake
+Get-ChildItem "C:\Users\adampaternostro\Desktop\Publix\" | 
+Foreach-Object {
+    
+
+    $fileToUpload = $_.FullName
+    $dataLakePath = "/" + $destinationDatalakefolder + "/" + $_.Name
+    $write =  "BEGIN Upload: " + $fileToUpload
+    Write-Output $write
+    Import-AzureRmDataLakeStoreItem -AccountName $destinationDataLakeAccount -Path $fileToUpload -Destination $dataLakePath -Force
+    $write =  "END   Upload: " + $fileToUpload
+    Write-Output $write
+
+}
